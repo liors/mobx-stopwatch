8813dc78babaa7253e859b6a6e3bd122ed89adab
Lior Shefer <lior.shefer@wix.com>
2017-07-18 22:34:03 +0300

    move to observer react class

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -3,64 +3,65 @@ import {observer} from 'mobx-react';
 import TimerDisplay from './TimerDisplay';
 import TimerAction from './TimerAction'
 
-const Main = observer(({timerStore}) => {
-  let firstButton;
-  let secondButton;
-    
-  if (!timerStore.isRunning) {
-    secondButton = (
-        <TimerAction style={{color: '#4bd761'}} event={timerStore.startTimer.bind(timerStore)} text= {'start'}/>
-    );
+@observer class Main extends React.Component {
+    render() {
+        let leftButton;
+        let rightButton;
+            
+        if (!this.props.timerStore.isRunning) {
+            rightButton = (
+                <TimerAction style={{color: '#4bd761'}} event={this.props.timerStore.startTimer.bind(this.props.timerStore)} text= {'start'}/>
+            );
 
-    firstButton = (
-        <TimerAction event={timerStore.resetTimer.bind(timerStore)} text= {'reset'}/>     
-    );
+            leftButton = (
+                <TimerAction event={this.props.timerStore.resetTimer.bind(this.props.timerStore)} text= {'reset'}/>     
+            );
 
-    if (!timerStore.hasStarted) {
-      firstButton = null;
+            if (!this.props.timerStore.hasStarted) {
+                leftButton = null;
+            }
+        } else {
+            rightButton = (
+                <TimerAction style={{color: '#fd3d2a'}} event={this.props.timerStore.stopTimer.bind(this.props.timerStore)} text= {'stop'}/>      
+            );
+
+            leftButton = (
+                <TimerAction event={this.props.timerStore.lapTimer.bind(this.props.timerStore)} text= {'lap'}/>  
+            );
+        }
+        return (
+            <div style={{fontSize: 30}}>
+            <div
+                style={{
+                background: 'white',
+                height: 120,
+                fontSize: 60,
+                fontFamily: 'HelveticaNeue-UltraLight',
+                border: 'solid #cecfd0',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'}}
+            >
+                {this.props.timerStore.mainDisplay}
+            </div>
+            <div>
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                {leftButton}
+                {rightButton}
+                </div>
+                <div>
+                {this.props.timerStore.lapData.map((el) =>
+                    <TimerDisplay
+                    key={el.lap.id}
+                    timer={el.lap}
+                    text={el.text}
+                    />
+                    )}
+                </div>
+            </div>
+            </div>
+        );
     }
-  } else {
-    secondButton = (
-        <TimerAction style={{color: '#fd3d2a'}} event={timerStore.stopTimer.bind(timerStore)} text= {'stop'}/>      
-    );
-
-    firstButton = (
-        <TimerAction event={timerStore.lapTimer.bind(timerStore)} text= {'lap'}/>  
-    );
-  }
-
-  return (
-    <div style={{fontSize: 30}}>
-      <div
-        style={{
-          background: 'white',
-          height: 120,
-          fontSize: 60,
-          fontFamily: 'HelveticaNeue-UltraLight',
-          border: 'solid #cecfd0',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'}}
-      >
-        {timerStore.mainDisplay}
-      </div>
-      <div>
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-          {firstButton}
-          {secondButton}
-        </div>
-        <div>
-          {timerStore.lapData.map((el) =>
-            <TimerDisplay
-              key={el.lap.id}
-              timer={el.lap}
-              text={el.text}
-            />
-            )}
-        </div>
-      </div>
-    </div>
-  );
-});
+}
 
 export default Main;
