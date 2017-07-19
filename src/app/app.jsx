d205c25c41ab762f7ca7ac19b00f375def265e80
Lior Shefer <lior.shefer@wix.com>
2017-07-19 14:14:50 +0300

    base dev setup w/ main component

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,13 +1,9 @@
-import {observable} from 'mobx';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main';
-import TimerStore from './TimerStore';
 
-const timerStore = new TimerStore();
 
 ReactDOM.render(
-  <Main timerStore={timerStore} />,
+  <Main />,
 document.getElementById('app')
 );
